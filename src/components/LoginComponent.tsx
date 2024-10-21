b09c51cbdae00ba9f6d589cc5902acf5e489cf4c
Jonas Balbino <jnb.dev@outlook.com>
2024-10-21 15:34:19 -0300

    Adding some Login logic

---
+++ src/components/LoginComponent.tsx
@@ -0,0 +1,94 @@
+import React, { useState } from 'react';
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+//@ts-ignore
+import SwaggerClient from 'swagger-client';
+
+// Form to send to API
+interface LoginForm {
+  email: string;
+  password: string;
+}
+
+const LoginComponent: React.FC = () => {
+  const [form, setForm] = useState<LoginForm>({ email: '', password: '' });
+  const [errorMessage, setErrorMessage] = useState<string | null>(null);
+  const [loading, setLoading] = useState<boolean>(false);
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = e.target;
+    setForm((prevForm) => ({ ...prevForm, [name]: value }));
+  };
+
+  // Handle login
+  const handleLogin = async () => {
+    setLoading(true);
+    setErrorMessage(null); 
+
+    try {
+      const client = await SwaggerClient({ url: '../../swagger.json' });
+
+      // Call the login API with the form data
+      const response = await client.apis.auth.auth_login({
+        Login: {
+          email: form.email,
+          password: form.password,
+        },
+      });
+
+      const authToken = response.data.access_token;
+      console.log('Login successful, token:', authToken);
+
+      // Storing authToken on local sotorage on browser
+      localStorage.setItem('authToken', authToken);
+
+      // Redirecting after login 
+      window.location.href = '/home';  // Test using Home
+    } catch (error) {
+      console.error('Login failed:', error);
+      setErrorMessage('Invalid email or password. Please try again.');
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  return (
+    <div>
+      <h2>Login</h2>
+
+      <div>
+        <label>Email:</label>
+        <input
+          type="email"
+          name="email"
+          value={form.email}
+          onChange={handleChange}
+          placeholder="Digite seu email"
+          required
+        />
+      </div>
+
+      <div>
+        <label>Password:</label>
+        <input
+          type="password"
+          name="password"
+          value={form.password}
+          onChange={handleChange}
+          placeholder="Digite sua senha"
+          required
+        />
+      </div>
+
+      <div>
+        <button onClick={handleLogin} disabled={loading}>
+          {loading ? 'Logging in...' : 'Entrar'}
+        </button>
+      </div>
+
+      {/* Error Message */}
+      {errorMessage && <p style={{ color: 'red' }}>{errorMessage}</p>}
+    </div>
+  );
+};
+
+export default LoginComponent;
