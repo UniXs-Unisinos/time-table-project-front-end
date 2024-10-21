e8a44b43eb17798a40ce0e187c8b80a475d7fa65
Jonas Pinheiro <jonasespinheiro@gmail.com>
2024-10-21 14:16:54 -0200

    Jonasedpi
    Alterar erros de digitção

--- src/routes/ForgotPassword.tsx
+++ src/routes/ForgotPassword.tsx
@@ -1,12 +1,12 @@
 import React from "react";
 
-const ForgotPassWord = () => {
+const ForgotPassword = () => {
     return (
     <>
-        <h1>ForgotPassword</h1>
-        <input type="email" name="email_user" id="email_user"/>
+        <p>Digite o seu e-mail para criar nova senha</p>
+        <p>E-mail: <input type="email" name="email_user" id="email_user"/></p>
     </>
     )
 };
 
-export default ForgotPassWord
+export default ForgotPassword
--- src/routes/Home.tsx
+++ src/routes/Home.tsx
@@ -1,11 +1,12 @@
 import React from "react";
 import NewPassword from "../components/NewPassword";
+import ForgotPassword from "../routes/ForgotPassword";
 
 const Home = () =>{
     return (
     <div> <h1>home</h1>
         <p>esqueci a senha</p>
-        <NewPassword/>
+        <ForgotPassword/>
     </div>
         
 );
